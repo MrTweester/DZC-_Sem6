a8ded0a50f8b5c0edc9d7f39f437ee65c5a6dd3c
Islam Dagaev <spy_002@bk.ru>
2023-05-30 20:58:19 +0500

    дз

---
+++ zad1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int[] FillArrayWithRandomNumber(int size)
+{
+    int[] arr = new int[size];
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine("Введите число массива: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(", ", array));
+int num=0;
+
+for (int i=0; i<array.Length; i++)
+{
+    if(array[i]<0)
+    {
+        num++;
+    }
+}
+System.Console.WriteLine(num);
